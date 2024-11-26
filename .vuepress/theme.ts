0b01c0b800cc72d671a9aa2d6c722b34271aa334
loctran016 <locphuc016@gmail.com>
2024-11-26 14:39:30 +0000

    :sparkles: Feature(custom): favicon
    add favicon, prepare for PWA support

--- .vuepress/theme.ts
+++ .vuepress/theme.ts
@@ -14,253 +14,257 @@ export default hopeTheme(
         displayFooter: true,
 
         blogLocales: {
-          tutorial: "Tutorial",
+            tutorial: "Tutorial",
         },
 
         metaLocales: {
-          editLink: "Edit this page on GitHub",
+            editLink: "Edit this page on GitHub",
         },
 
-    hostname: "https://titieo016.github.io/notable",
+        hostname: "https://loctran016.github.io/notable",
 
-    author: {
-      name: "LocTran016",
-    //   url: "https://your.domain",
-    },
+        author: {
+            name: "LocTran016",
+            //   url: "https://your.domain",
+        },
 
-    iconAssets: "fontawesome-with-brands",
+        favicon: "/favicon.ico",
+
+        iconAssets: "fontawesome-with-brands",
 
         // logo: "",
-    // TODO: add logo
+        // TODO: add logo
 
-    repo: "titieo016/notable",
-    // docsDir: "blog",
+        repo: "loctran016/notable",
+        // docsDir: "blog",
 
         blog: {
-        description: "A YDS-er that enjoy Programming 🧑‍💻 <br> & Gym 💪",
+            description: "A YDS-er that enjoy Programming 🧑‍💻 <br> & Gym 💪",
             intro: "/intro.html",
-          avatar: 'logo.jpg',
-      medias: {
-        Discord: "https://example.com",
-        // Dribbble: "https://example.com",
-        Email: "mailto:ttloc.y24@ump.edu.vn",
-        // Evernote: "https://example.com",
-        Facebook: "https://facebook.com/trantanloc016",
-        // Flipboard: "https://example.com",
-        GitHub: "https://github.com/titieo016",
-        // Gitlab: "https://example.com",
-        // Gmail: "mailto:info@example.com",
-        // Instagram: "https://example.com",
-        // Linkedin: "https://example.com",
+            avatar: 'logo.jpg',
+            medias: {
+                Discord: "https://example.com",
+                // Dribbble: "https://example.com",
+                Email: "mailto:ttloc.y24@ump.edu.vn",
+                // Evernote: "https://example.com",
+                Facebook: "https://facebook.com/trantanloc016",
+                // Flipboard: "https://example.com",
+                GitHub: "https://github.com/loctran016",
+                // Gitlab: "https://example.com",
+                // Gmail: "mailto:info@example.com",
+                // Instagram: "https://example.com",
+                // Linkedin: "https://example.com",
 
-        // Pocket: "https://example.com",
-        // Rss: "https://example.com",
-        // Steam: "https://example.com",
-        // VuePressThemeHope: {
-        //   icon: "https://theme-hope-assets.vuejs.press/logo.svg",
-        //   link: "https://theme-hope.vuejs.press",
-        // },
-      },
+                // Pocket: "https://example.com",
+                // Rss: "https://example.com",
+                // Steam: "https://example.com",
+                // VuePressThemeHope: {
+                //   icon: "https://theme-hope-assets.vuejs.press/logo.svg",
+                //   link: "https://theme-hope.vuejs.press",
+                // },
+            },
         },
 
 
-// fullscreen: true,
+        // fullscreen: true,
 
-    encrypt: {
-      config: {
-        // "/demo/encrypt.html": ["1234"],
-        // "/zh/demo/encrypt.html": ["1234"],
-      },
+        encrypt: {
+            config: {
+                // "/demo/encrypt.html": ["1234"],
+                // "/zh/demo/encrypt.html": ["1234"],
+            },
+        },
+
+        // enable it to preview all changes in time
+        // hotReload: true,
+
+        plugins: {
+            blog: {
+                excerptLength: 0,
+                type: [
+                    //   {
+                    //     key: "tutorial",
+                    //     filter: (page): boolean =>
+                    //       Boolean(page.filePathRelative?.includes("demo/")),
+                    //     sorter: (pageA, pageB): number =>
+                    //       dateSorter(pageA.frontmatter.date, pageB.frontmatter.date),
+                    //     layout: "BlogType",
+                    //   },
+                ],
+            },
+
+            // install @waline/client before enabling it
+            // WARNING: This is a test server for demo only.
+            // You should create and use your own comment service in production.
+            // comment: {
+            //   provider: "Waline",
+            //   serverURL: "https://waline-comment.vuejs.press",
+            // },
+
+            components: {
+                components: ["Badge", "VPCard", "PDF", 'Share'],
+                componentOptions: {
+
+                    share: {
+
+                        //   colorful: true,
+                        services: ['email', 'facebook', 'messenger', 'telegram'],
+                        colorful: true,
+                        inline: true,
+                    }
+                }
+                //   componentsOptions: {},
+            },
+            searchPro: {
+                autoSuggestions: false
+            },
+
+            markdownImage: {
+                figure: true,
+                lazyload: true,
+                size: true,
+            },
+
+            // install katex or before enabling it
+            markdownMath: {
+                type: "mathjax", // or "mathjax"
+                output: "chtml",
+            },
+
+            markdownTab: true,
+
+            // markdownMath: true,
+            comment: {
+                provider: "Giscus",
+                // repo: 'notable', repoId: '', category: '',categoryId:''
+                repo: "loctran016/notable",
+                repoId: "R_kgDONUFZ3A",
+                category: "Q&A",
+                categoryId: "DIC_kwDONUFZ3M4CklO9",
+                mapping: "title",
+                lightTheme: 'light_protanopia',
+                darkTheme: 'dark_protanopia',
+                // data-strict:"0",
+                // data-emit-metadata:"1",
+                // inputPosition:"bottom"
+                // lang:"en",
+            },
+
+
+            // All features are enabled for demo, only preserve features you need here
+            mdEnhance: {
+                align: true,
+                attrs: true,
+                component: true,
+                mark: true,
+                spoiler: true,
+                markmap: true,
+                // stylize: [
+                //   {
+                //     matcher: "Recommended",
+                //     replacer: ({ tag }) => {
+                //       if (tag === "em")
+                //         return {
+                //           tag: "Badge",
+                //           attrs: { type: "tip" },
+                //           content: "Recommended",
+                //         };
+                //     },
+                //   },
+                // ],
+                // TODO: Check back on this later
+                sub: true,
+                sup: true,
+                tasklist: true,
+                // vPre: true,
+
+                // install chart.js before enabling it
+                // chart: true,
+
+                // insert component easily
+
+                // install echarts before enabling it
+                // echarts: true,
+
+                // install flowchart.ts before enabling it
+                // flowchart: true,
+
+                // gfm requires mathjax-full to provide tex support
+                // gfm: true,
+
+                // install mermaid before enabling it
+                // mermaid: true,
+
+                // playground: {
+                //   presets: ["ts", "vue"],
+                // },
+
+                // install @vue/repl before enabling it
+                // vuePlayground: true,
+            },
+
+            // uncomment these if you want a PWA
+            pwa: {
+                favicon: "/favicon.ico",
+                cacheHTML: true,
+                cacheImage: true,
+                appendBase: true,
+                apple: {
+                    icon: "/assets/apple-touch-icon.png",
+                    statusBarColor: "black",
+                },
+                // msTile: {
+                //   image: "/assets/ms-icon-144.png",
+                //   color: "#ffffff",
+                // },
+                manifest: {
+                    icons: [
+                        {
+                            src: "/assets/web-app-manifest-512.png",
+                            sizes: "512x512",
+                            purpose: "maskable",
+                            type: "image/png",
+                        },
+                        {
+                            src: "/assets/web-app-manifest-192x192.png",
+                            sizes: "192x192",
+                            purpose: "maskable",
+                            type: "image/png",
+                        },
+                        // {
+                        //   src: "/assets/icon/chrome-512.png",
+                        //   sizes: "512x512",
+                        //   type: "image/png",
+                        // },
+                        // {
+                        //   src: "/assets/icon/chrome-192.png",
+                        //   sizes: "192x192",
+                        //   type: "image/png",
+                        // },
+                    ],
+                    //   shortcuts: [
+                    //     {
+                    //       name: "Demo",
+                    //       short_name: "Demo",
+                    //       url: "/demo/",
+                    //       icons: [
+                    //         {
+                    //           src: "/assets/icon/guide-maskable.png",
+                    //           sizes: "192x192",
+                    //           purpose: "maskable",
+                    //           type: "image/png",
+                    //         },
+                    //       ],
+                    //     },
+                    //   ],
+                },
+
+                // install @vuepress/plugin-revealjs before enabling it
+                // revealjs: {
+                //   plugins: ["highlight", "math", "search", "notes", "zoom"],
+                // },
+            },
+        },
     },
-
-    // enable it to preview all changes in time
-    // hotReload: true,
-
-    plugins: {
-      blog: {excerptLength:0,
-        type: [
-        //   {
-        //     key: "tutorial",
-        //     filter: (page): boolean =>
-        //       Boolean(page.filePathRelative?.includes("demo/")),
-        //     sorter: (pageA, pageB): number =>
-        //       dateSorter(pageA.frontmatter.date, pageB.frontmatter.date),
-        //     layout: "BlogType",
-        //   },
-        ],
-      },
-
-      // install @waline/client before enabling it
-      // WARNING: This is a test server for demo only.
-      // You should create and use your own comment service in production.
-      // comment: {
-      //   provider: "Waline",
-      //   serverURL: "https://waline-comment.vuejs.press",
-      // },
-
-      components: {
-          components: ["Badge", "VPCard", "PDF",'Share'],
-          componentOptions: {
-
-              share: {
-
-                //   colorful: true,
-                  services: ['email', 'facebook', 'messenger', 'telegram'],
-                  colorful: true,
-                  inline: true,
-              }
-          }
-        //   componentsOptions: {},
-        },
-        searchPro: {
-          autoSuggestions: false
-      },
-
-      markdownImage: {
-        figure: true,
-        lazyload: true,
-        size: true,
-      },
-
-      // install katex or before enabling it
-      markdownMath: {
-          type: "mathjax", // or "mathjax"
-          output: "chtml",
-      },
-
-        markdownTab: true,
-
-        // markdownMath: true,
-        comment: {
-            provider: "Giscus",
-            // repo: 'notable', repoId: '', category: '',categoryId:''
-                    repo:"titieo/notable",
-    repoId:"R_kgDONUFZ3A",
-        category:"Q&A",
-        categoryId:"DIC_kwDONUFZ3M4CklO9",
-            mapping: "title",
-            lightTheme: 'light_protanopia',
-        darkTheme: 'dark_protanopia',
-       // data-strict:"0",
-        // data-emit-metadata:"1",
-        // inputPosition:"bottom"
-        // lang:"en",
-        },
-
-
-      // All features are enabled for demo, only preserve features you need here
-      mdEnhance: {
-        align: true,
-        attrs: true,
-        component: true,
-        mark: true,
-        spoiler: true,
-        markmap: true,
-        // stylize: [
-        //   {
-        //     matcher: "Recommended",
-        //     replacer: ({ tag }) => {
-        //       if (tag === "em")
-        //         return {
-        //           tag: "Badge",
-        //           attrs: { type: "tip" },
-        //           content: "Recommended",
-        //         };
-        //     },
-        //   },
-          // ],
-        // TODO: Check back on this later
-        sub: true,
-        sup: true,
-        tasklist: true,
-        // vPre: true,
-
-        // install chart.js before enabling it
-        // chart: true,
-
-        // insert component easily
-
-        // install echarts before enabling it
-        // echarts: true,
-
-        // install flowchart.ts before enabling it
-        // flowchart: true,
-
-        // gfm requires mathjax-full to provide tex support
-        // gfm: true,
-
-        // install mermaid before enabling it
-        // mermaid: true,
-
-        // playground: {
-        //   presets: ["ts", "vue"],
-        // },
-
-        // install @vue/repl before enabling it
-        // vuePlayground: true,
-      },
-
-      // uncomment these if you want a PWA
-      // pwa: {
-      //   favicon: "/favicon.ico",
-      //   cacheHTML: true,
-      //   cacheImage: true,
-      //   appendBase: true,
-      //   apple: {
-      //     icon: "/assets/icon/apple-icon-152.png",
-      //     statusBarColor: "black",
-      //   },
-      //   msTile: {
-      //     image: "/assets/icon/ms-icon-144.png",
-      //     color: "#ffffff",
-      //   },
-      //   manifest: {
-      //     icons: [
-      //       {
-      //         src: "/assets/icon/chrome-mask-512.png",
-      //         sizes: "512x512",
-      //         purpose: "maskable",
-      //         type: "image/png",
-      //       },
-      //       {
-      //         src: "/assets/icon/chrome-mask-192.png",
-      //         sizes: "192x192",
-      //         purpose: "maskable",
-      //         type: "image/png",
-      //       },
-      //       {
-      //         src: "/assets/icon/chrome-512.png",
-      //         sizes: "512x512",
-      //         type: "image/png",
-      //       },
-      //       {
-      //         src: "/assets/icon/chrome-192.png",
-      //         sizes: "192x192",
-      //         type: "image/png",
-      //       },
-      //     ],
-      //     shortcuts: [
-      //       {
-      //         name: "Demo",
-      //         short_name: "Demo",
-      //         url: "/demo/",
-      //         icons: [
-      //           {
-      //             src: "/assets/icon/guide-maskable.png",
-      //             sizes: "192x192",
-      //             purpose: "maskable",
-      //             type: "image/png",
-      //           },
-      //         ],
-      //       },
-      //     ],
-      //   },
-
-      // install @vuepress/plugin-revealjs before enabling it
-      // revealjs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-    },
-  },
   { custom: true },
 );
--- posts/hoack.md
+++ posts/hoack.md
@@ -6,6 +6,7 @@ excerpt: Thi CK 60 câu - 70 phút
 # cover: '/hoack-bg.avif'
 tags: ['Hoá Y']
 # draft: true
+star: 1
 ---
 
 ## Chương 1: Cấu tạo nguyên tử - Định luật tuần hoàn
@@ -13,6 +14,96 @@ tags: ['Hoá Y']
 ## Chương 2: Cấu tạo phân tử - Liên kết hoá học
 
 ## Chương 3: Nhiệt động lực hoá học
+
+### Khái niệm cơ bản
+
+1. Nhiệt sinh (nhiệt tạo thành)
+Là hiệu ứng nhiệt tạo thành 1 mol chất từ các ĐƠN CHẤT tự do BỀN VỮNG nhất ở điều kiện tiêu chuẩn ĐẲNG ÁP 25°C, 1 atm ➜ nhiệt sinh CHUẨN hay enthalpy chuẩn ➜ ΔH°₂₉₈s or ΔH°₂₉₈f **O<sub>2 (k)</sub> có nhiệt sinh chuẩn là 0 vì O<sub>2</sub> tồn tại ở dạng khí ở dktc**
+2. Nhiệt phân hủy: Là hiệu ứng nhiệt của 1 hợp chất phân hủy 1 mol chất đó thành ĐƠN CHẤT BỀN ➜ ΔH°ph
+3. Nhiệt cháy
+Là hiệu ứng nhiệt đốt cháy 1 mol chất đó bằng oxy để tạo thành sản phẩm cháy BỀN ở ĐẲNG ÁP ➜ ΔH°c
+
+MỘT SỐ LƯU Ý (ENTHALPY) ➜ H
+
+Nội năng: ΔU=Q+A=U₂-U₁
+
+$Q=m×c×ΔT$
+
+* m: khối lượng [g]
+* c: nhiệt dung riêng [J/(g.K)]
+* ΔT: sự thay đổi nhiệt độ kelvin trược và sau phản ứng Bomb nhiệt lượng:
+
+Túi chườm nóng ➜ CaCl₂
+Túi chườm lạnh ➜ NH₄NO₃
+
+### ĐỊNH LUẬT HESS VÀ HỆ QUẢ
+
+1. Hiệu ứng nhiệt bẳng tổng quá trình trung gian
+ΔH°pư = ΣΔH°(các quá trình trung gian)
+2. NHIỆT PHÂN HỦY bằng giá trị của NHIỆT SINH nhưng NGƯỢC DẤU
+ΔH°(thuận)=‒ΔH°(nghịch)
+3. Hiệu ứng nhiệt bằng tổng NHIỆT SINH sản phẩm trừ tác chất
+ΔH°pư = ΣnΔH°f(sản phẩm) ‒ ΣmΔH°f(tác chất)
+4. Hiệu ứng nhiệt bằng tổng NHIỆT CHÁY tác chất trừ sản phẩm
+ΔH°pư = ΣnΔH°c(tác chất) ‒ ΣmΔH°c(sản phẩm)
+5. Hiệu ứng nhiệt bằng tổng năng lượng liên kết bị ĐỨT trừ tổng năng lượng liên kết được RÁP
+ΔH°pư = ΣnE(đứt-tác chất) ‒ ΣmE(ráp-sản phẩm)
+
+### MỘT SỐ LƯU Ý (ENTROPY) ➜ S
+
+Đo lường năng lượng tiêu hao (sự chuyển động của phân tử bằng nhiệt)➜$s=q/t$
+
+Đo sự hỗn độn (mất trật tự)➜
+$S=k.ln(W)$
+
+* k: hằng số Boltzmann (k=1,38×10⁻²³J/K)
+* W: số lượng vi trạng thái microstates➜ ex: W=1(hệ đứng im),S=0
+
+⇒ S có xu hướng TĂNG khi T(nhiệt độ), V(thể tích), η(số phân tử chuyển động độc lập) TĂNG
+
+⇒ S DƯƠNG khi n(sản phẩm)> n(ban đầu) và ưu tiên chất KHÍ EX: CaCO₃(r) ➜ CaO(r) + CO₂(k) S dương
+      2CO(k) + O₂(k) ➜ 2CO₂(k) S âm
+
+
+### CÔNG THỨC
+
+1. Biến đổi entropy trong HỆ (system) ➜ rxn or sys
+ ΔS°₂₉₈ = ΣnΔS°(sản phẩm) ‒ ΣmΔS°(tác chất)
+
+2. Mối liên hệ giữa entropy và enthalpy
+ ΔS(môi trường) = ΔH(môi trường)/T = -ΔH(hệ)/T
+
+3. Entropy vũ trụ (universe)
+ΔS(vũ trụ) = ΔS(môi trường) + ΔS(hệ)
+ΔS(vũ trụ)>0 ➜ tự diễn biến
+
+⇒ tự diễn biến (KHÔNG thuận nghịch) ➜ ΔS(vũ trụ) TĂNG. tự diễn biến (thuận nghịch) ➜ ΔS(vũ trụ) KHÔNG THAY ĐỔI
+4. Năng lượng tự do Gibbs (ΔG)
+ΔG° = ΔH° ‒ T.ΔS°     (hệ)➜sys or rxn ΔG° = ‒T.ΔS°              (vũ trụ)➜universe ⇒ΔG<0 ➜ tự diễn biếnΔG>0 ➜ không tự diễn biến
+
+```markmap
+#### TỎA NHIỆT (ΔH<0)
+* ΔS>0 ➜ ΔG<0 bất kể T°
+
+* ΔS<0 ➜ ΔG<0 ở T° thấp, ΔG>0 khi |T.ΔS| > |ΔH|
+
+#### THU NHIỆT (ΔH>0)
+* ΔS>0 ➜ ΔG>0 ở T° THẤP, ΔG<0 ở khi |T.ΔS| > |ΔH|
+
+* ΔS<0 ➜ ΔG>0 bất kể T°
+```
+
+
+### CÂN BẰNG HÓA HỌC
+
+Cân bằng theo chiều làm GIẢM các yếu tố TĂNG
+
+
+$P.V=nRT R=0,082 (lít×atm)/(mol×K)$
+
+* p đo bằng atm, V đo bằng lít
+
+* R= 8,3145 J/(mol×K) ➜ p đo bằng pascal, V đo bằng m³
 
 ## Chương 4: Động hoá học
 
